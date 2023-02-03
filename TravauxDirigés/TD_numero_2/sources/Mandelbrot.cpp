aa5bae24a4ab515c08a858893daf419ee024081a
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2023-02-03 10:59:49 +0100

    mandelbrot mpi

--- TravauxDirigés/TD_numero_2/sources/Mandelbrot.cpp
+++ TravauxDirigés/TD_numero_2/sources/Mandelbrot.cpp
@@ -1,11 +1,11 @@
-# include <iostream>
-# include <cstdlib>
-# include <string>
-# include <chrono>
-# include <cmath>
-# include <vector>
-# include <fstream>
-
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <chrono>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <mpi.h>
 
 /** Une structure complexe est définie pour la bonne raison que la classe
  * complex proposée par g++ est très lente ! Le calcul est bien plus rapide
@@ -14,51 +14,55 @@
 struct Complex
 {
     Complex() : real(0.), imag(0.)
-    {}
+    {
+    }
     Complex(double r, double i) : real(r), imag(i)
-    {}
-    Complex operator + ( const Complex& z )
     {
-        return Complex(real + z.real, imag + z.imag );
     }
-    Complex operator * ( const Complex& z )
+    Complex operator+(const Complex &z)
     {
-        return Complex(real*z.real-imag*z.imag, real*z.imag+imag*z.real);
+        return Complex(real + z.real, imag + z.imag);
     }
-    double sqNorm() { return real*real + imag*imag; }
-    double real,imag;
+    Complex operator*(const Complex &z)
+    {
+        return Complex(real * z.real - imag * z.imag, real * z.imag + imag * z.real);
+    }
+    double sqNorm() { return real * real + imag * imag; }
+    double real, imag;
 };
 
-std::ostream& operator << ( std::ostream& out, const Complex& c )
+std::ostream &operator<<(std::ostream &out, const Complex &c)
 {
-  out << "(" << c.real << "," << c.imag << ")" << std::endl;
-  return out;
+    out << "(" << c.real << "," << c.imag << ")" << std::endl;
+    return out;
 }
 
 /** Pour un c complexe donné, calcul le nombre d'itérations de mandelbrot
  * nécessaires pour détecter une éventuelle divergence. Si la suite
  * converge, la fonction retourne la valeur maxIter
  **/
-int iterMandelbrot( int maxIter, const Complex& c)
+int iterMandelbrot(int maxIter, const Complex &c)
 {
-    Complex z{0.,0.};
+    Complex z{0., 0.};
     // On vérifie dans un premier temps si le complexe
     // n'appartient pas à une zone de convergence connue :
     // Appartenance aux disques  C0{(0,0),1/4} et C1{(-1,0),1/4}
-    if ( c.real*c.real+c.imag*c.imag < 0.0625 )
+    if (c.real * c.real + c.imag * c.imag < 0.0625)
         return maxIter;
-    if ( (c.real+1)*(c.real+1)+c.imag*c.imag < 0.0625 )
+    if ((c.real + 1) * (c.real + 1) + c.imag * c.imag < 0.0625)
         return maxIter;
-    // Appartenance à la cardioïde {(1/4,0),1/2(1-cos(theta))}    
-    if ((c.real > -0.75) && (c.real < 0.5) ) {
-        Complex ct{c.real-0.25,c.imag};
+    // Appartenance à la cardioïde {(1/4,0),1/2(1-cos(theta))}
+    if ((c.real > -0.75) && (c.real < 0.5))
+    {
+        Complex ct{c.real - 0.25, c.imag};
         double ctnrm2 = sqrt(ct.sqNorm());
-        if (ctnrm2 < 0.5*(1-ct.real/ctnrm2)) return maxIter;
+        if (ctnrm2 < 0.5 * (1 - ct.real / ctnrm2))
+            return maxIter;
     }
     int niter = 0;
     while ((z.sqNorm() < 4.) && (niter < maxIter))
     {
-        z = z*z + c;
+        z = z * z + c;
         ++niter;
     }
     return niter;
@@ -69,70 +73,141 @@ int iterMandelbrot( int maxIter, const Complex& c)
  * translation et homothétie une valeur complexe c qui servira pour
  * itérer sur la suite de Mandelbrot. Le nombre d'itérations renvoyé
  * servira pour construire l'image finale.
- 
+
  Sortie : un vecteur de taille W*H avec pour chaque case un nombre d'étape de convergence de 0 à maxIter
  MODIFICATION DE LA FONCTION :
  j'ai supprimé le paramètre W étant donné que maintenant, cette fonction ne prendra plus que des lignes de taille W en argument.
  **/
-void 
-computeMandelbrotSetRow( int W, int H, int maxIter, int num_ligne, int* pixels)
+void computeMandelbrotSetRow(int W, int H, int maxIter, int num_ligne, int *pixels)
 {
     // Calcul le facteur d'échelle pour rester dans le disque de rayon 2
     // centré en (0,0)
-    double scaleX = 3./(W-1);
-    double scaleY = 2.25/(H-1.);
+    double scaleX = 3. / (W - 1);
+    double scaleY = 2.25 / (H - 1.);
     //
     // On parcourt les pixels de l'espace image :
-    for ( int j = 0; j < W; ++j ) {
-       Complex c{-2.+j*scaleX,-1.125+ num_ligne*scaleY};
-       pixels[j] = iterMandelbrot( maxIter, c );
+    for (int j = 0; j < W; ++j)
+    {
+        Complex c{-2. + j * scaleX, -1.125 + num_ligne * scaleY};
+        pixels[j] = iterMandelbrot(maxIter, c);
     }
 }
 
 std::vector<int>
-computeMandelbrotSet( int W, int H, int maxIter )
+computeMandelbrotSet(int W, int H, int localH, int offsetH, int maxIter, bool clock)
 {
     std::chrono::time_point<std::chrono::system_clock> start, end;
-    std::vector<int> pixels(W*H);
-    start = std::chrono::system_clock::now();
+
+    std::vector<int> pixels(W * localH);
+
+    if (clock)
+        start = std::chrono::system_clock::now();
     // On parcourt les pixels de l'espace image :
-    for ( int i = 0; i < H; ++i ) {
-      computeMandelbrotSetRow(W, H, maxIter, i, pixels.data() + W*(H-i-1) );
+    for (int i = 0; i < localH; ++i)
+    {
+        computeMandelbrotSetRow(W, H, maxIter, offsetH + i, pixels.data() + W * (localH - i - 1));
     }
-    end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end-start;
-    std::cout << "Temps calcul ensemble mandelbrot : " << elapsed_seconds.count() 
-              << std::endl;
+
+    if (clock)
+    {
+        end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        std::cout << "Temps calcul ensemble mandelbrot : " << elapsed_seconds.count()
+                  << std::endl;
+    }
     return pixels;
 }
 
 /** Construit et sauvegarde l'image finale **/
-void savePicture( const std::string& filename, int W, int H, const std::vector<int>& nbIters, int maxIter )
+void savePicture(const std::string &filename, int W, int H, const std::vector<int> &nbIters, int maxIter)
 {
-    double scaleCol = 1./maxIter;//16777216
-    std::ofstream ofs( filename.c_str(), std::ios::out | std::ios::binary );
+    double scaleCol = 1. / maxIter; // 16777216
+    std::ofstream ofs(filename.c_str(), std::ios::out | std::ios::binary);
     ofs << "P6\n"
         << W << " " << H << "\n255\n";
-    for ( int i = 0; i < W * H; ++i ) {
-        double iter = scaleCol*nbIters[i];
-        unsigned char r = (unsigned char)(256 - (unsigned (iter*256.) & 0xFF));
-        unsigned char b = (unsigned char)(256 - (unsigned (iter*65536) & 0xFF));
-        unsigned char g = (unsigned char)(256 - (unsigned( iter*16777216) & 0xFF));
+    for (int i = 0; i < W * H; ++i)
+    {
+        double iter = scaleCol * nbIters[i];
+        unsigned char r = (unsigned char)(256 - (unsigned(iter * 256.) & 0xFF));
+        unsigned char b = (unsigned char)(256 - (unsigned(iter * 65536) & 0xFF));
+        unsigned char g = (unsigned char)(256 - (unsigned(iter * 16777216) & 0xFF));
         ofs << r << g << b;
     }
     ofs.close();
 }
 
-int main(int argc, char *argv[] ) 
- { 
+int main(int argc, char *argv[])
+{
+    MPI_Comm global;
+    int rank, nbp;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_dup(MPI_COMM_WORLD, &global);
+    MPI_Comm_size(global, &nbp);
+    MPI_Comm_rank(global, &rank);
+
+    std::cout << std::endl;
+
+    int localH = 100;
+
     const int W = 800;
-    const int H = 600;
+    const int H = localH * nbp;
+
     // Normalement, pour un bon rendu, il faudrait le nombre d'itérations
     // ci--dessous :
-    //const int maxIter = 16777216;
-    const int maxIter = 8*65536;
-    auto iters = computeMandelbrotSet( W, H, maxIter );
-    savePicture("mandelbrot.tga", W, H, iters, maxIter);
+    // const int maxIter = 16777216;
+    const int maxIter = 8 * 65536;
+
+    // ======================== NO MPI ============================
+
+    if (rank == 0)
+    {
+        std::cout << "Mandelbrot without MPI" << std::endl;
+
+        auto no_mpi = computeMandelbrotSet(W, H, H, 0, maxIter, true);
+
+        savePicture("mandelbrot_no_mpi.tga", W, H, no_mpi, maxIter);
+    }
+
+    // ======================== MPI ============================
+
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+
+    std::vector<int> offsetH;
+    offsetH.resize(1);
+    std::vector<int> globalData;
+    std::vector<int> finalIters;
+
+    if (rank == 0)
+    {
+        std::cout << "Mandelbrot with MPI" << std::endl;
+
+        start = std::chrono::system_clock::now();
+        finalIters.resize(W * H);
+        globalData.resize(nbp);
+        for (int i = 0; i < nbp; ++i)
+        {
+            globalData[nbp - i - 1] = i * localH;
+        }
+    }
+
+    MPI_Scatter(globalData.data(), 1, MPI_INT, offsetH.data(), 1, MPI_INT, 0, global);
+
+    std::cout << "[" << rank << "] : localH " << localH << ", offset " << offsetH.at(0) << std::endl;
+
+    auto iters = computeMandelbrotSet(W, H, localH, offsetH.at(0), maxIter, false);
+
+    MPI_Gather(iters.data(), localH * W, MPI_INT, finalIters.data(), localH * W, MPI_INT, 0, global);
+
+    if (rank == 0)
+    {
+        end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        std::cout << "Temps calcul ensemble mandelbrot : " << elapsed_seconds.count()
+                  << std::endl;
+        std::cout << "Image is " << W << "x" << finalIters.size() / W << std::endl;
+        savePicture("mandelbrot_mpi.tga", W, H, finalIters, maxIter);
+    }
+
+    MPI_Finalize();
     return EXIT_SUCCESS;
- }
-    
+}
